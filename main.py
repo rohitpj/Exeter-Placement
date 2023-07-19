a25f08ec7bd008dc33c07fc8d5ee6104b6cccbf8
rohitpj <rohitpauljohn@gmail.com>
2023-07-19 18:19:21 +0100

    bruh

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ from numpy.random import normal as normrnd
 from scipy.linalg import khatri_rao as kr_prod
 from scipy.stats import wishart
 from scipy.stats import invwishart
+from scipy.stats import multivariate_normal as mvn
 from numpy.linalg import solve as solve
 from numpy.linalg import cholesky as cholesky_lower
 from scipy.linalg import cholesky as cholesky_upper
 from scipy.linalg import solve_triangular as solve_ut
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+from scipy.stats import expon as exp
 
 #stole all these functions hehehe
 
@@ -39,3 +41,72 @@ def mvnrnd_pre(mu, Lambda):
 def cov_mat(mat, mat_bar):
     mat = mat - mat_bar
     return mat.T @ mat
+
+def gibbs_sampling(U, V, Imax):
+    # Placeholder for the update equations
+    def update(U, V):
+        # Implement the update equations from the paper here
+        return U, V
+
+    for i in range(Imax):
+        U, V = update(U, V)
+    return np.dot(U, V)
+
+def update_hyper(U, V, Imax):
+    print("update_hyper")
+
+def BPMF(dense_mat, sparse_mat, init, rank, burn_iter, gibbs_iter):
+    #rohits
+    max_iter = burn_iter + gibbs_iter
+    dim1, dim2 = sparse_mat.shape
+    U = np.random.rand(dim1, rank)
+    V = np.random.rand(rank, dim2)
+    E = np.zeros((dim1, dim2))
+
+    mu_U = np.zeros(r)  # Mean for the Gaussian prior over U
+    lambda_U = np.eye(r)  # Precision matrix for the Gaussian prior over U
+
+    mu_V = np.zeros(r)  # Mean for the Gaussian prior over V
+    ambda_V = np.eye(r)  # Precision matrix for the Gaussian prior over V
+
+    def exponential_prior(U, V, Gamma_U, Gamma_V):
+        U_prior = np.prod([exp.pdf(U[i, :], scale=1/Gamma_U) for i in range(dim1)])
+        V_prior = np.prod([exp.pdf(V[:, j], scale=1/Gamma_V) for j in range(dim2)])
+        return U_prior, V_prior
+    
+
+    def gaussian_prior(U, V, mu_U, lambda_U, mu_V, lambda_V):
+        U_prior = np.prod([mvn.pdf(U[i, :], mu_U, lambda_U) for i in range(dim1)])
+        V_prior = np.prod([mvn.pdf(V[:, j], mu_V, lambda_V) for j in range(dim2)])
+        return U_prior, V_prior
+    
+def exponential_prior(U, V, Gamma_U, Gamma_V,dim1, dim2):
+    U_prior = np.prod([exp.pdf(U[i, :], scale=1/Gamma_U) for i in range(dim1)])
+    V_prior = np.prod([exp.pdf(V[:, j], scale=1/Gamma_V) for j in range(dim2)])
+    return U_prior, V_prior
+
+
+def gaussian_prior(U, V, mu_U, lambda_U, mu_V, lambda_V,dim1, dim2):
+    U_prior = np.prod([mvn.pdf(U[i, :], mu_U, lambda_U) for i in range(dim1)])
+    V_prior = np.prod([mvn.pdf(V[:, j], mu_V, lambda_V) for j in range(dim2)])
+    return U_prior, V_prior
+
+def test_gaussian_prior():
+    dim1, dim2, rank = 10, 20, 5  # just some test values
+    U = np.random.rand(dim1, rank)
+    V = np.random.rand(rank, dim2)
+
+    mu_U = np.zeros(rank)  # Mean for the Gaussian prior over U
+    lambda_U = np.eye(rank)  # Precision matrix for the Gaussian prior over U
+
+    mu_V = np.zeros(rank)  # Mean for the Gaussian prior over V
+    lambda_V = np.eye(rank)  # Precision matrix for the Gaussian prior over V
+    
+    U_prior, V_prior = gaussian_prior(U, V, mu_U, lambda_U, mu_V, lambda_V,dim1, dim2)
+
+    # Test if U_prior and V_prior are in valid probability range (0-1)
+    assert 0 <= U_prior <= 1, "U_prior is out of valid probability range (0-1)"
+    assert 0 <= V_prior <= 1, "V_prior is out of valid probability range (0-1)"
+    
+test_gaussian_prior()
+print("yay")
